60be09a5f6f446fc20e4a199a49bf200c8e83c86
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-01-09 18:35:09 +0300

    submitted exercise 7

---
+++ arrays/exerccise7/main.js
@@ -0,0 +1,10 @@
+let colors = ["Red", "Green", "Yellow",  "Blue",];
+
+
+console.log("adding a color at the end");
+colors.push("Black");
+console.log(colors);
+
+console.log("Removing the first color");
+colors.shift();
+console.log(colors);
