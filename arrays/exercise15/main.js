3ea104c0b65ab18304c971efa91eb1878732ad50
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-01-12 17:18:00 +0300

    submited exercise 15

---
+++ arrays/exercise15/main.js
@@ -0,0 +1,13 @@
+
+
+const people = [
+    {name: "Abdi", age: 24, City: "mogadisho"},
+    {name: "mohamed", age: 22, city: "Laascanod"},
+    {name:  "farah", age: 28, city: "london"}
+]
+
+for(let p of people){
+
+    console.log(p);
+    
+}
