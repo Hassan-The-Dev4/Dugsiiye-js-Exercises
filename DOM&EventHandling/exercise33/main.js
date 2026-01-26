f70244b57448b56134472d5c608b5b8ac22070ba
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-01-26 13:50:27 +0300

    dom manupluation exercise

---
+++ DOM&EventHandling/exercise33/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+function changeHeader(){
+
+    const header = document.querySelector('.header');
+    header.textContent = "Welcome to the Innovation Center";
+}
+
+function changeElement(){
+
+    const text = document.querySelector('.text');
+    text.innerHTML = `We can help you, and we solve your <strong>problems</strong>`;
+}
