08ddaec585b93ce4b0046c515a5606d12656073e
hassangnt18@gmail.com <hassangnt18@gmail.com>
2026-02-09 16:38:45 +0300

    sumitted exercise 38

---
+++ DOM&EventHandling/responsive-nav/app.js
@@ -0,0 +1,11 @@
+
+
+const toggleButton = document.querySelector('.toggle-button');
+const navbar =  document.querySelector('.navbar');
+
+
+toggleButton.addEventListener("click", function(){
+
+    navbar.classList.toggle("active");
+   
+})
